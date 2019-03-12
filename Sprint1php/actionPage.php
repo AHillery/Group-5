3b9da29f75a36f9c7b53634330bcf1791ae2e530
jjent425 <48250158+jjent425@users.noreply.github.com>
2019-03-12 08:39:10 -0500

    Add files via upload

--- Sprint1php/actionPage.php
+++ Sprint1php/actionPage.php
@@ -1,48 +1,52 @@
 <?php
-
 require_once("Template.php");
 require_once("DB.class.php");
 $page = new Template("Action Page");
 $page->addHeadElement("<link rel='stylesheet' href='style.css'>");
 $page->finalizeTopSection();
 $page->finalizeBottomSection();
-
 $db = new DB();
-
-
-if (!$db->getConnStatus()) {
+if (!$db->getConnStatus()) 
+{
   print "An error has occurred with connection\n";
   exit;
 }
-$ip=$_SERVER['REMOTE_ADDR'];
-$major = $db->dbEsc($_POST['major']);
-$grade = $db->dbEsc($_POST['grade']);
-$favPizzaTopping = $db->dbEsc($_POST['favPizzaTopping']);
-$query = 'INSERT INTO survey VALUES (0,now(),"'.$major.'","'.$grade.'","'.$favPizzaTopping.'","'.$ip.'",0)';
-
-$result = $db->dbCall($query);
-
-
-print $page->getTopSection();
-if((isset($major) && isset($grade) && isset($favPizzaTopping)) && ($major != "" && $favPizzaTopping != ""))
+if((isset($_POST['major']) && isset($_POST['grade']) && isset($_POST['favPizzaTopping'])))
 {
-print	"<header>";
-print		"<h1>Survey Submitted</h1>";
-print	"</header>";
+	$ip=$_SERVER['REMOTE_ADDR'];
+	$major = $db->dbEsc($_POST['major']);
+	$grade = $db->dbEsc($_POST['grade']);
+	$favPizzaTopping = $db->dbEsc($_POST['favPizzaTopping']);
 	
-print	"<!-- this shows if you submitted the form  -->";
-print	"<p id = 'notification'>Thank you for participating in our survey!!!</p>";
+	if(($_POST['major'] != "" && $_POST['favPizzaTopping'] != ""))
+	{
+	
+	$query = 'INSERT INTO survey VALUES (0,now(),"'.$major.'","'.$grade.'","'.$favPizzaTopping.'","'.$ip.'",0)';
+	$result = $db->dbCall($query);
+	print $page->getTopSection();
+
+	print	"<header>";
+	print		"<h1>Survey Submitted</h1>";
+	print	"</header>";
+		
+	print	"<!-- this shows if you submitted the form  -->";
+	print	"<p id = 'notification'>Thank you for participating in our survey!!!</p>";
+	}
+	else
+	{
+		if($major == "")
+		{
+			print"<p>ERROR: MAJOR FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</p>";
+		}
+		else if($favPizzaTopping == "")
+		{
+				print"<p>ERROR: FAVORITE PIZZA TOPPING FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</p>";
+		}
+	}
 }
-else{
-	if($major == ""){
-		print"<p>ERROR: MAJOR FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</p>";
-	}
-	else if($favPizzaTopping == ""){
-		print"<p>ERROR: FAVORITE PIZZA TOPPING FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</p>";
-	}
-	else{
-		print"<p>ERROR: A FEILD WAS NOT SET</p>";
-	}
+else
+{
+	print"<p>ERROR: NO FORM SUBMITTED</p>";
 }
 print $page->getBottomSection();
-
+?>
