7e46c96c2b67068bc9963d34fb8a6d778efcdc18
AHillery <48097599+AHillery@users.noreply.github.com>
2019-03-12 08:52:12 -0500

    Add files via upload

--- Sprint1php/actionPage.php
+++ Sprint1php/actionPage.php
@@ -1,52 +1,105 @@
+
 <?php
+
 require_once("Template.php");
+
 require_once("DB.class.php");
+
 $page = new Template("Action Page");
+
 $page->addHeadElement("<link rel='stylesheet' href='style.css'>");
+
 $page->finalizeTopSection();
+
 $page->finalizeBottomSection();
+
 $db = new DB();
+
+print $page->getTopSection();
+
 if (!$db->getConnStatus()) 
+
 {
+
   print "An error has occurred with connection\n";
+
   exit;
+
 }
+
 if((isset($_POST['major']) && isset($_POST['grade']) && isset($_POST['favPizzaTopping'])))
+
 {
+
 	$ip=$_SERVER['REMOTE_ADDR'];
+
 	$major = $db->dbEsc($_POST['major']);
+
 	$grade = $db->dbEsc($_POST['grade']);
+
 	$favPizzaTopping = $db->dbEsc($_POST['favPizzaTopping']);
+
 	
+
 	if(($_POST['major'] != "" && $_POST['favPizzaTopping'] != ""))
+
 	{
+
 	
+
 	$query = 'INSERT INTO survey VALUES (0,now(),"'.$major.'","'.$grade.'","'.$favPizzaTopping.'","'.$ip.'",0)';
+
 	$result = $db->dbCall($query);
-	print $page->getTopSection();
+
+	
+
+
 
 	print	"<header>";
+
 	print		"<h1>Survey Submitted</h1>";
+
 	print	"</header>";
+
 		
+
 	print	"<!-- this shows if you submitted the form  -->";
+
 	print	"<p id = 'notification'>Thank you for participating in our survey!!!</p>";
+
 	}
+
 	else
+
 	{
+
 		if($major == "")
+
 		{
-			print"<p>ERROR: MAJOR FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</p>";
+
+			print"<h1>ERROR: MAJOR FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</h1>";
+
 		}
+
 		else if($favPizzaTopping == "")
+
 		{
-				print"<p>ERROR: FAVORITE PIZZA TOPPING FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</p>";
+
+				print"<h1>ERROR: FAVORITE PIZZA TOPPING FIELD WAS LEFT BLANK WHEN FORM WAS ENETERD</h1>";
+
 		}
+
 	}
+
 }
+
 else
+
 {
-	print"<p>ERROR: NO FORM SUBMITTED</p>";
+
+	print"<h1>ERROR: NO FORM SUBMITTED</h1>";
+
 }
+
 print $page->getBottomSection();
-?>
+
