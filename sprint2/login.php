a906f9df11abd0ea706329ea41e499998fc9bd78
bberg512 <48096186+bberg512@users.noreply.github.com>
2019-04-09 09:39:07 -0500

    Add files via upload

---
+++ sprint2/login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once("Template.php");
+
+$page = new Template("LogIn");
+$page->addHeadElement("<link rel='stylesheet' href='style.css'>");
+$page->finalizeTopSection();
+$page->finalizeBottomSection();
+
+print $page->getTopSection();
+print  "<form name='myForm' action='homePage.php' method='POST'>";
+print  "<fieldset>";
+print  "<label for='username'>Username</label>";
+print  "<input type='text' name='username' value=''><br>";
+print  "<label for='Password'>Password</label>";
+print  "<input type='password' name='password' value=''><br>";
+print  "<input type='submit' id='submit' name='submit' value='Log In'>";
+print  "</fieldset>";
+print  "</form>";
+
+print $page->getBottomSection();
+?>
