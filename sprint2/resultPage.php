552d262f0df0d5a31a980344d03a35bbc2912009
bberg512 <48096186+bberg512@users.noreply.github.com>
2019-04-15 13:51:13 -0500

    Update resultPage.php

--- sprint2/resultPage.php
+++ sprint2/resultPage.php
@@ -6,7 +6,7 @@ require_once("DB.class.php");
 
 $page = new Template("Result Page");
 
-$page->addHeadElement("<link rel='stylesheet' href='style.css'>");
+$page->addHeadElement("<link rel='stylesheet' href='styles.css'>");
 
 $page->finalizeTopSection();
 
@@ -36,9 +36,26 @@ if(isset($_POST['lookup']) && $_POST['lookup']!="")
 
 	print $page->getTopSection();
 print "<header class='header'>";
-print	  "<h1>Result From Album Search</h1>";
+print	"<nav>";
+print		"<ul>";
+print			"<li><a href='homePage.php'>Home</a></li>";
+print			"<li><a href='surveyPage.php'>Survey</a></li>";
+print			"<li><a href='albumPage.php'>Album Search</a></li>";
+print			"<li><a href='privacyPolicyPage.php'>Privacy Policy</a></li>";
+if(isset($_SESSION['role']) && ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'admin and user')){
+	print			"<li><a href='surveyDataPage.php'>Survey Data</a></li>";
+}
+if(!isset($_SESSION['loggedin']))
+{
+	print	"<a href = 'login.php' id = 'logInAndOut'>Log In</a>";
+}
+else
+{
+	print	"<a href = 'logout.php' id = 'logInAndOut'>Log out</a>";
+}
+print	  	"</ul>";
+print	"</nav>";
 print  "</header>";
-
 
 
 	if(!empty($result))
