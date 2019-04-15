9e1f45f03695ba85eaa480be9a6a250613550398
bberg512 <48096186+bberg512@users.noreply.github.com>
2019-04-15 13:48:03 -0500

    Update login.php

--- sprint2/login.php
+++ sprint2/login.php
@@ -3,11 +3,19 @@ session_start();
 require_once("Template.php");
 
 $page = new Template("LogIn");
-$page->addHeadElement("<link rel='stylesheet' href='style.css'>");
+$page->addHeadElement("<link rel='stylesheet' href='styles.css'>");
 $page->finalizeTopSection();
 $page->finalizeBottomSection();
 
 print $page->getTopSection();
+print	"<header  class='header'>";
+print  "<nav  class='header'>";
+print		"<ul>";
+print			"<li><a href='homePage.php'>Home</a></li>";
+print		"</ul>";
+print "</nav>";
+print	"</header>";
+print "<div class='content'>";
 print  "<form name='myForm' action='errorPage.php' method='POST'>";
 print  "<fieldset>";
 print  "<label for='username'>Username</label>";
@@ -17,6 +25,6 @@ print  "<input type='password' name='password' value=''><br>";
 print  "<input type='submit' id='submit' name='submit' value='Log In'>";
 print  "</fieldset>";
 print  "</form>";
-
+print  "</div>";
 print $page->getBottomSection();
 ?>
