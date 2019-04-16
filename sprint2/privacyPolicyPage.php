9f6efe6735f437870f683ef6cdf1df791a04699a
bberg512 <48096186+bberg512@users.noreply.github.com>
2019-04-16 06:42:03 -0500

    Update privacyPolicyPage.php

--- sprint2/privacyPolicyPage.php
+++ sprint2/privacyPolicyPage.php
@@ -33,53 +33,47 @@ else
 }
 
 
-print		"</ul>";
-print	"</nav>";
-print  "</header>";
-print  "<!-- this is the private policy page from www.wisconsin.edu/privacy-policy/ -->";
-print	"<div class='wrap'>"; 
 print "<div class='content'>";
-print "<div class='text'>";
-print		"<p>&nbsp;&nbsp;&nbsp;The University of Wisconsin System Administration (UWSA) recognizes the 
+print "<div class='fieldset' id='policyFieldset'>";
+print		"<p>&nbsp;&nbsp;&nbsp;The University of Wisconsin System Administration (UWSA) recognizes the
 			importance of protecting the privacy of information provided to us.
 			</p>";
 print "</div>";
-print "<div class='text'>";
-print	"<h3>Personal information</h3>";
-print			"<p>&nbsp;&nbsp;&nbsp;We will use personal information that you provide via e-mail or through other 
-			online means only for purposes necessary to serve your needs, such as responding 
-			to an inquiry or other request for information. This may involve redirecting your 
+print "<div class='fieldset' id='policyFieldset'>";
+print	"<h1>Personal information</h1>";
+print			"<p>&nbsp;&nbsp;&nbsp;We will use personal information that you provide via e-mail or through other
+			online means only for purposes necessary to serve your needs, such as responding
+			to an inquiry or other request for information. This may involve redirecting your
 			inquiry or comment to another person or department better suited to meeting your needs.
-			Some webpages at UWSA may collect personal information about visitors and use that 
-			information for purposes other than those stated above. Each webpage that collects 
-			information will have a separate privacy statement that will tell you how that 
+			Some webpages at UWSA may collect personal information about visitors and use that
+			information for purposes other than those stated above. Each webpage that collects
+			information will have a separate privacy statement that will tell you how that
 			information is used.
 			</p>";
 
- 
+
 print "</div>";
-print "<div class='text'>";
-print		"<h3>Collected Information</h3>";
+print "<div class='fieldset' id='policyFieldset'>";
+print		"<h1>Collected Information</h1>";
 print			"<p>&nbsp;&nbsp;&nbsp;UWSA monitors network traffic for the purposes of site management and security.
-			We use this information to help diagnose problems and carry out other administrative 
-			tasks.We also use statistic information to determine which information is of most 
-			interest to users, to identify system problem areas, or to help determine technical 
+			We use this information to help diagnose problems and carry out other administrative
+			tasks.We also use statistic information to determine which information is of most
+			interest to users, to identify system problem areas, or to help determine technical
 			requirements.The server log information does not include personal information.
 			</p>";
 print "</div>";
-print "<div class='text'>";
-print		"<h3>External websites</h3>";
-print			"<p>&nbsp;&nbsp;&nbsp;This site contains links to other sites outside of UWSA. UWSA is not responsible 
+print "<div class='fieldset' id='policyFieldset'>";
+print		"<h1>External websites</h1>";
+print			"<p>&nbsp;&nbsp;&nbsp;This site contains links to other sites outside of UWSA. UWSA is not responsible
 			for the privacy practices or the content of such websites.
 			</p>";
 print "</div>";
-print "<div class='text'>";
-print		"<h3>Questions</h3>";
+print "<div class='fieldset' id='policyFieldset'>";
+print		"<h1>Questions</h1>";
 print			"<p>&nbsp;&nbsp;&nbsp;If you have any questions about this privacy statement, the practices of this site,
 			or your use of this website, please contact Webmaster.
 			</p>";
 print "</div>";
-print "</div>";
-print	"</div>";	
+print "</div>";	
 print $page->getBottomSection();
 
