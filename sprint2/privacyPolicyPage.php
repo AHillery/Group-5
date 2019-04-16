973715a2b249c0ac1a0ecac2adf21320b2b977c6
bberg512 <48096186+bberg512@users.noreply.github.com>
2019-04-16 03:08:15 -0500

    Update privacyPolicyPage.php

--- sprint2/privacyPolicyPage.php
+++ sprint2/privacyPolicyPage.php
@@ -16,6 +16,7 @@ print		"<ul>";
 print			"<li><a href='homePage.php'>Home</a></li>";
 print			"<li><a href='surveyPage.php'>Survey</a></li>";
 print			"<li><a href='albumPage.php'>Album Search</a></li>";
+print			"<li><a href='privacyPolicyPage.php'>Privacy Policy</a></li>";
 
 if(isset($_SESSION['role']) && ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'admin and user')){
 	print			"<li><a href='surveyDataPage.php'>Survey Data</a></li>";
